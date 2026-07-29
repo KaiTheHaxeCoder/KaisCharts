e344a317f13610256ede6676abd5fa1dfa9039a3
Brainy7980 <bpurdy12@icloud.com>
2026-07-29 14:02:42 -0700

    Update script.lua

--- data/garden-of-hopes-and-dreams/script.lua
+++ data/garden-of-hopes-and-dreams/script.lua
@@ -3,19 +3,19 @@ function onStepHit()
         doTweenZoom('zoom', 'hud', 2, 3, 'sineInOut')
     end
     if curStep == 88 then
-        doTweenZoom('zoom', 'hud', 1, 1, 'sineInOut')
+        doTweenZoom('zoom', 'hud', 0.75, 1, 'sineInOut')
     end
     if curStep == 96 then
-        doTweenZoom('zoom', 'hud', 1, 0.1, 'elasticIn')
+        doTweenZoom('zoom', 'hud', 1, 0.1, 'sineInOut')
     end
 
-    if curStep == 126 then
+    if curStep == 124 then
         doTweenZoom('zoom', 'hud', 2, 3, 'sineInOut')
     end
     if curStep == 152 then
-        doTweenZoom('zoom', 'hud', 1, 1, 'sineInOut')
+        doTweenZoom('zoom', 'hud', 0.5, 1, 'sineInOut')
     end
-    if curStep == 160 then
-        doTweenZoom('zoom', 'hud', 1, 0.1, 'elasticIn')
+    if curStep == 158 then
+        doTweenZoom('zoom', 'hud', 1, 0.1, 'sineInOut')
     end
 end
